574b92d7e9194f9085a1c32e6d4a7430509a7c35
Wade Costa <wadeccosta@gmail.com>
2022-09-15 10:42:44 -0700

    first commit

---
+++ src/ApplicationMain.java
@@ -0,0 +1,20 @@
+public class ApplicationMain {
+    
+    public static void main(String[] args) {
+        String[] shopList = new String[] {
+                "Apples",
+                "Bananas",
+                "Corn",
+                "Dog Food",
+                "Cereal", 
+                "Bread", 
+                "Eggs",
+                "Milk"
+        };
+        
+        for (String item: shopList) {
+            System.out.println(item);
+        }    
+       }
+ 
+}
